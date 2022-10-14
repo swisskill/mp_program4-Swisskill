982f3f9dc644b801d845afb0bb83c4d94816337e
swisskill <wbrant@uwyo.edu>
2022-10-14 16:05:40 -0600

    I-dunno;pushForFun

--- expenses/app/src/main/java/com/example/expenses/expend.java
+++ expenses/app/src/main/java/com/example/expenses/expend.java
@@ -30,106 +30,38 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Objects;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class expend extends Fragment {
 
     final static String TAG = "Expend Fragment";
-    // DatabaseReference myChildRef; //ATTN:
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        ((MainActivity) requireActivity()).getSupportActionBar().hide();
+        Objects.requireNonNull(((MainActivity) requireActivity()).getSupportActionBar()).hide();//get rid of toolbar
         View myView = inflater.inflate(R.layout.fragment_expend, container, false);
         FloatingActionButton fab = myView.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "-wb", Toast.LENGTH_LONG).show();
+                //but actually spawn dialogue box
             }
         });
         return myView;
     }
-//    void updateDialog(final Note note) {
-//        LayoutInflater inflater = LayoutInflater.from(requireContext());
-//        final View textenter = inflater.inflate(R.layout.fragment_my_dialog, null);
-//        final EditText et_note = textenter.findViewById(R.id.et_note);
-//        et_note.setText(note.getNote());
-//        final EditText et_title = textenter.findViewById(R.id.et_title);
-//        et_title.setText(note.getTitle());
-//        //final AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(requireContext(), R.style.ThemeOverlay_AppCompat_Dialog)); //ATTN: WHAT THEME? WHERE
-//        final AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(requireContext(), R.style.Theme_Expenses)); //ATTN: WHAT THEME? WHERE
-//        builder.setView(textenter).setTitle("Update Note");
-//        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
-//
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int id) {
-//
-//                        logthis("update title is " + et_title.getText().toString());
-//                        logthis("update note is " + et_note.getText().toString());
-//                        //myChildRef.push().setValue(new Note(et_title.getText().toString(), et_note.getText().toString()));
-//                        Note mynote = new Note(et_title.getText().toString(), et_note.getText().toString());
-//
-//                        // mFirebaseDatabaseReference.child("messages").child(note.getId()).setValue(mynote);
-//                        //OR since it's partially there already, use myChildRef
-//                        //myChildRef.child(note.getId()).setValue(mynote); //ATTN: WHY IS DB NOT IMPORTING
-//                        //Toast.makeText(getBaseContext(), userinput.getText().toString(), Toast.LENGTH_LONG).show();
-//
-//                    }
-//                })
-//                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        logthis("dialog canceled");
-//                        dialog.cancel();
-//
-//                    }
-//                });
-//        //you can create the dialog or just use the now method in the builder.
-//        //AlertDialog dialog = builder.create();
-//        //dialog.show();
-//        builder.show();
-//    }
-//
-//    /**
-//     * Add dialog, with blanks.  then adds the data into the database.
-//     */
-//    void showDialog(String title) {
-//        LayoutInflater inflater = LayoutInflater.from(requireContext());
-//        final View textenter = inflater.inflate(R.layout.fragment_my_dialog, null);
-//        final EditText et_note = textenter.findViewById(R.id.et_note);
-//        final EditText et_title = textenter.findViewById(R.id.et_title);
-//        final AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(requireContext(), R.style.Theme_Expenses)); //ATTN: HOW TO MAKE ThemeOverlay_AppCompat_Dialog
-//        builder.setView(textenter).setTitle(title);
-//        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-//
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int id) {
-//
-//                        logthis("title is " + et_title.getText().toString());
-//                        logthis("note is " + et_note.getText().toString());
-//                        //Note push is used only when added new data for child "notes".  ie needes a unique id.
-//                        //myChildRef.push().setValue(new Note(et_title.getText().toString(), et_note.getText().toString())); //WHY IS DB NOT IMPORTING
-//                        //Toast.makeText(getBaseContext(), userinput.getText().toString(), Toast.LENGTH_LONG).show();
-//                    }
-//                })
-//                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        logthis("dialog canceled");
-//                        dialog.cancel();
-//
-//                    }
-//                });
-//        //you can create the dialog or just use the now method in the builder.
-//        //AlertDialog dialog = builder.create();
-//        //dialog.show();
-//        builder.show();
-//    }
-//
-//
-//    void logthis(String item) {
-//        Log.d(TAG, "Value is: " + item);
-//    }
-//
+
+    void showDialog() {
+        LayoutInflater inflater = LayoutInflater.from(requireContext());
+        final View textenter = inflater.inflate(R.layout.fragment_my_dialog, null);
+        final EditText et_note = textenter.findViewById(R.id.et_note);
+        final EditText et_title = textenter.findViewById(R.id.et_title);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(requireContext(), androidx.appcompat.R.style.Base_Theme_AppCompat_Dialog));
+        builder.setView(textenter).setTitle("Add");
+
+    }
 }
